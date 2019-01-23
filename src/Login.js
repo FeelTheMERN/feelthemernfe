50b37d1808d3eac4c24df8397372d798da510511
Lili Molloy <liliana.molloy@gmail.com>
2019-01-23 14:17:25 +1100

    fixed loggin redirection

--- src/Login.js
+++ src/Login.js
@@ -10,8 +10,9 @@ class Login extends Component {
     }
 
     //redirect() redirects the user to their home page based on the user role
-    redirect(isAdmin) {
+    redirect = (isAdmin) => {
         console.log(isAdmin)
+        console.log(this.props)
         if(isAdmin) return this.props.history.push('/admin/home')
         this.props.history.push('user/home')
     }
@@ -22,7 +23,7 @@ class Login extends Component {
         e.preventDefault();
         let url
         if(window.location.pathname === "/admin") url = "http://localhost:5000/admin/";
-        if(window.location.pathname === "/login") url = "http://localhost:5000/user/login";
+        if(window.location.pathname === "/") url = "http://localhost:5000/user/login";
 
         const { username, password } = this.state        
         const data = { username, password }
@@ -30,31 +31,24 @@ class Login extends Component {
         axios.post(url, data)
             .then(resp => { // save token to local storage
                 const { token, isAdmin } = resp.data
-                console.log(isAdmin)
                 localStorage.setItem('token', token)
                 this.setState({message: 'You are logged in', error: null})
                 this.redirect(isAdmin)
                 })
-            .catch(err => {
-                const { status } = err.response
-                    if (status === 403) {
-                        this.setState({error: 'Incorrect username and password', message: null})
-                    }
-                    console.log(err.response)
-                })
+            .catch(err => console.log(err))
     }
 
     render() {
         const {error, message} = this.state
         return (
             <>
-                    <form>
-                        <label htmlFor="username">Username:</label>
-                        <input type="text" id="username" onChange={this.handleInputChange}/>
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" onChange={this.handleInputChange}/>
-                        <button onClick={this.submitForm}>Login</button>
-                    </form>
+                <form>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" onChange={this.handleInputChange}/>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" onChange={this.handleInputChange}/>
+                    <button onClick={this.submitForm}>Login</button>
+                </form>
                 { error && <p>{error}</p>}
                 { message && <p>{message}</p>}
             </>
